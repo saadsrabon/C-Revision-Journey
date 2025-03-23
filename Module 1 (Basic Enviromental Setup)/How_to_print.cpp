34fe2b95dd9b4b07b412470b4a56510dd1ef3888
Saad Srabon <saadsrabon2@gmail.com>
2025-03-23 09:02:17 +0600

    how to print added

---
+++ Module 1 (Basic Enviromental Setup)/How_to_print.cpp
@@ -0,0 +1,10 @@
+#include<iostream> // This line is for standard input and output
+using namespace std;
+int main() // from here our main function starts
+{
+    std::cout << "Hello World" << std::endl; 
+    // easy form
+    cout << "Hello World" << endl;
+    //this is how 
+    return 0;
+}
