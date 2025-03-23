6366b040a6693037a640e14fc13146a6db3ed386
Saad Srabon <saadsrabon2@gmail.com>
2025-03-23 09:04:49 +0600

    how to take input file added

---
+++ Module 1 (Basic Enviromental Setup)/How_to_take_input.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+ 
+
+int main() {
+
+ int age; // variable declaration
+
+ char initial;
+
+ 
+
+ cout <<"Enter your age:";
+
+ cin >> age; // input
+
+ 
+
+ cout << "Enter the first letter of your name: "; 
+
+ cin >> initial;
+
+ 
+
+ cout << age << endl;
+
+ cout << initial << endl;// output
+
+ 
+
+ return 0;
+
+}
