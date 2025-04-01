41dcaacd7aebd66e70b8e4842137cf39897fda9b
Saad Srabon <saadsrabon2@gmail.com>
2025-04-01 11:47:27 +0600

    capacity file added in module-5

--- Module 5/String.cpp
+++ Module 5/String.cpp
@@ -1,1 +1,12 @@
 // Here we will practice String in C++
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    string name ;
+  
+    cout<<"Please enter your name"<<endl;
+    cin>>name;
+    cout << "Eid Mubarak "<<name<<endl;
+    return 0;
+}
---
+++ Module 5/StringCapacity.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+//     //
+//   Here we will practice String in C++ and its capacity
+//     s.size() -> returns the size of the string.
+// s.max_size() -> returns the maximum size that string can hold.
+// s.capacity() -> returns current available capacity of the string.
+// s.clear() -> clear the string.
+// s.empty() -> return true/false if the string is empty.
+// s.resize() -> change the size of the string.
+
+    return 0;
+}
